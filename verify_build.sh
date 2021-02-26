7385387a225ef10721d7848958c9b8a1da1e994a
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-26 15:27:12 +0000

    chore(): initial commit

---
+++ verify_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update;
+sudo apt install -y git docker.io disorderfs
+
+sudo groupadd docker;
+sudo usermod -aG docker $USER
+
+git clone https://gitlab.com/walletscrutiny/walletScrutinyCom
+
+cd walletScrutinyCom
+
+./test.sh ./vault/3.6.1.apk
